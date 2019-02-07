6609542c909450f7af2cd61ea1328a74dab6603a
Damian Dziura <dymekk208j@gmail.com>
2019-02-07 22:38:38 +0100

    Add project files.

---
+++ LearningBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearningBox.MvcApplication" Language="C#" %>
---
+++ LearningBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningBox.Startup))]
+namespace LearningBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
